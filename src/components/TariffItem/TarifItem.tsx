024a161cb3e53eaf4b93064f8ee4cfb1b21bc40f
kviron <kviron.web.dev@gmail.com>
2022-02-23 21:11:37 +0300

    поправил пути к картинкам

--- src/components/TariffItem/TarifItem.tsx
+++ src/components/TariffItem/TarifItem.tsx
@@ -6,29 +6,80 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {FC} from "react";
+import Grid from '@mui/material/Grid';
 
 const TariffItem: FC<any> = () => {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image="@/public/tariff1.jpg"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Первый тариф
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Небольшое описание тарифа его цены и других характеристик
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height="140"
+                            image="/tariff1.jpg"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Первый тариф
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Небольшое описание тарифа его цены и других характеристик
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height="140"
+                            image="/tariff1.jpg"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Второй тариф
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Небольшое описание тарифа его цены и других характеристик
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height="140"
+                            image="/tariff1.jpg"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Третий тариф
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Небольшое описание тарифа его цены и других характеристик
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
